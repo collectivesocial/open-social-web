0912d83e6023cf805050083c113a06f130897afb
Brittany Ellich <brittanyellich@github.com>
2026-02-15 21:17:58 -0800

    Fix the error boundary ordering

--- src/main.tsx
+++ src/main.tsx
@@ -13,12 +13,12 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <Provider>
+    <Provider>
+      <ErrorBoundary>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </Provider>
-    </ErrorBoundary>
+      </ErrorBoundary>
+    </Provider>
   </StrictMode>,
 )
